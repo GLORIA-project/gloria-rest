c9a60a8ad132644debf72e66b1c565093dac276b
fserena <fserena@ciclope.info>
2013-12-08 23:39:16 +0100

    Users REST service

---
+++ src/main/java/eu/gloria/gs/services/api/resources/Users.java
@@ -0,0 +1,80 @@
+/**
+ * Author: Fernando Serena (fserena@ciclope.info)
+ * Organization: Ciclope Group (UPM)
+ * Project: GLORIA
+ */
+package eu.gloria.gs.services.api.resources;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.context.ApplicationContext;
+
+import eu.gloria.gs.services.api.data.UserDataAdapter;
+import eu.gloria.gs.services.api.data.dbservices.UserDataAdapterException;
+import eu.gloria.gs.services.api.security.ApplicationContextProvider;
+import eu.gloria.gs.services.core.client.GSClientProvider;
+
+/**
+ * @author Fernando Serena (fserena@ciclope.info)
+ * 
+ */
+
+@Path("/users")
+public class Users {
+
+	@Context
+	HttpServletRequest request;
+
+	private static UserDataAdapter userAdapter;
+
+	static {
+
+		ApplicationContext context = ApplicationContextProvider
+				.getApplicationContext();
+
+		String hostName = (String) context.getBean("hostName");
+		String hostPort = (String) context.getBean("hostPort");
+
+		GSClientProvider.setHost(hostName);
+		GSClientProvider.setPort(hostPort);
+
+		userAdapter = (UserDataAdapter) context.getBean("userDataAdapter");
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/authenticate")
+	public Response authenticateUser(@QueryParam("verify") boolean verify) {
+
+		if (verify) {
+			return Response.ok(new ArrayList<>()).build();
+		}
+
+		String user = null;
+		String password = null;
+
+		if (request.getAttribute("user") != null) {
+
+			user = (String) request.getAttribute("user");
+			password = (String) request.getAttribute("password");
+
+			GSClientProvider.setCredentials(user, password);
+		}
+
+		try {
+			String token = userAdapter.createToken(user, password);
+			return Response.ok(JSONConverter.toJSON(token)).build();
+		} catch (UserDataAdapterException e) {
+			return Response.serverError().entity(e.getMessage()).build();
+		}
+	}
+}
